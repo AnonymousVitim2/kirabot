e20b0facec48708f17d81b302f6fc30ffc019dbc
AnonymousVitim2 <lobootff3@gmail.com>
2021-02-10 07:48:31 -0400

    First commit

---
+++ src/toinmenu.js
@@ -0,0 +1,31 @@
+const vitimmenu = (prefix, pushname) => {
+    return `◪ *Comandos do vitim*
+╔════════════════════
+║╭──❉ *MENU DO TITIO PAIN* ❉── 
+║│➸${prefix}prefixo
+║│Sobre : esse muda o prefixo
+║╰───────────
+║╭───────────
+║│➸${prefix}block
+║│Sobre : esse da block
+║╰───────────
+║╭───────────
+║│➸${prefix}bc
+║│Sobre : esse da ban no numero usa naum
+║╰───────────
+║╭───────────
+║│➸${prefix}bcgc
+║│Sobre : esse da ban no numero de um geito diferente
+║╰───────────
+║╭───────────
+║│➸${prefix}clearall
+║│Sobre : esse limpa todas conversa do bot
+║╰───────────
+╠════════════════════
+║ TITIO VITIM O MAIS BRABOR
+╚════════════════════
+`
+
+}
+
+exports.vitimmenu = vitimmenu
